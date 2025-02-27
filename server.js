f9f0354390709099803e6b0306266952a76771df
Ashley Sajous <ashleysajous@macbookair.mynetworksettings.com>
2025-02-27 15:29:09 -0500

    Update

--- server.js
+++ server.js
@@ -191,6 +191,8 @@ io.on('connection', (socket) => {
 });
 
 // 14. Start the Server
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
   console.log(`🔥 Server running on port ${PORT}`);
 });
