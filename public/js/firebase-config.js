7503ee79d77105cc8078dccb97c1d1b442ca87af
Ashley Sajous <ashleysajous@macbookair.mynetworksettings.com>
2025-04-05 17:32:23 -0400

    update firebase

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,17 +1,14 @@
 // public/js/firebase-config.js
-
-// Your Firebase config
-// Initialize Firebase using the configuration from your .env or environment variables
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyDcNH3J0i7TsXP6XgEG862EQA72Lmrzcro",
+  authDomain: "jimmysdelivery-9261d.firebaseapp.com",
+  projectId: "jimmysdelivery-9261d",
+  storageBucket: "jimmysdelivery-9261d.firebasestorage.app",
+  messagingSenderId: "52478759963",
+  appId: "1:52478759963:web:11fd5ab98b7bfd841bd4be",
+  measurementId: "G-CW1LMVX7QN"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Assign global references
