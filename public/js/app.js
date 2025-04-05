70f0443fa0ab9499a6072565c45825c484837dc8
Ashley Sajous <ashleysajous@macbookair.mynetworksettings.com>
2025-04-05 17:02:42 -0400

    F

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,8 @@
-// Initialize Firebase App
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-};
-firebase.initializeApp(firebaseConfig);
+
+// app.js
+import { auth, db } from './firebase-config.js';
+
+// Use auth or db here
 
 // Firestore Database and Firebase Auth
 const db = firebase.firestore();
